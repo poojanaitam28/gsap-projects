4a80771f04991bca8f84811d38c76e75c7d6085f
poojanaitam28 <poojanaitam3@gmail.com>
2024-12-10 15:08:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const tl = gsap.timeline()
+
+tl.to("#page2 img", {
+    width:"90%",
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        // markers: true,
+        start: "top 0",
+        end: "top -100%",
+        scrub: 2,
+        pin:true,
+    },
+})
